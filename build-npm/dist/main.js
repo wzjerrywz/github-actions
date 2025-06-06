b57b2d58f4bae6a25ead58bf58d9546a56639dda
michael <wzjerrywz@163.com>
2025-06-06 15:52:00 +0800

    update

--- build-npm/dist/main.js
+++ build-npm/dist/main.js
@@ -37,73 +37,17 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const core = __importStar(require("@actions/core"));
-const exec = __importStar(require("@actions/exec"));
-const cmd_1 = require("./cmd");
 const path_1 = __importDefault(require("path"));
-const os_1 = __importDefault(require("os"));
 function validateInputs(params) {
-    if (!params.nvmVersion)
-        throw new Error('nvmVersion input is required');
-    if (!params.nodejsVersion)
-        throw new Error('nodejsVersion input is required');
     return params;
 }
 async function run() {
     try {
         const inputs = validateInputs({
-            nvmVersion: core.getInput('nvm-version', { required: true }),
-            nodejsVersion: core.getInput('nodejs-version', { required: true }),
+            projectPath: core.getInput('project-path', { required: true })
         });
-        const nvmDir = path_1.default.join(os_1.default.homedir(), '.nvm');
-        core.exportVariable('NVM_DIR', nvmDir);
-        const nvm = `curl -o install.sh https://raw.githubusercontent.com/nvm-sh/nvm/v${inputs.nvmVersion}/install.sh`;
-        await exec.exec(nvm, []);
-        await exec.exec('bash', ['install.sh']);
-        // 加载 NVM 环境
-        await exec.exec('bash', [
-            '-c',
-            `. ${nvmDir}/nvm.sh && nvm install ${inputs.nodejsVersion} && nvm use ${inputs.nodejsVersion} `
-        ]);
-        // 获取 Node.js 路径并添加到 PATH
-        const nodePath = await exec.getExecOutput('bash', [
-            '-c',
-            `. ${nvmDir}/nvm.sh && dirname $( nvm which ${inputs.nodejsVersion} ) `
-        ], {
-            silent: true
-        });
-        console.log(`nodePath: ${nodePath.stdout}`);
-        const nodeBinPath = path_1.default.join(nodePath.stdout.trim(), 'bin');
-        core.addPath(nodeBinPath);
-        const textGet = await (0, cmd_1.getText)('node', ['-v']);
-        console.log(textGet);
-        core.info(`Node.js Of by GetText:   ` + textGet);
-        await exec.exec('node', ['-v']);
-        // const url = 'https://download.java.net/java/GA/jdk17/0d483333a00540d886896bac774ff48b/35/GPL/openjdk-17_linux-x64_bin.tar.gz';
-        // await exec.exec('wget', ['-q', url]);
-        // await exec.exec('tar', ['-xf', 'openjdk-17_linux-x64_bin.tar.gz', '-C', './']);
-        // const jdkHome = await capture('pwd', [])  + '/jdk-17' ;
-        // await exec.exec('chmod', ['+x', `${jdkHome}/bin/java`]);
-        // 设置 JAVA_HOME 环境变量
-        // core.exportVariable('JAVA_HOME', jdkHome);
-        // 设置其他变量（如 PATH）
-        // core.exportVariable('PATH', `${jdkHome}/bin:${process.env.PATH}`);
-        // await exec.exec('java', ['-version']);
-        // await exec.exec('ls', ['./'])
-        // await exec.exec('npm', ['i', 'npm@latest'])
-        core.info(`Processing with: ${JSON.stringify(inputs)}`);
-        const result = {
-            original: inputs,
-            processedAt: new Date().toISOString(),
-            message: `Received ${inputs.nodejsVersion} with 1 selection`
-        };
-        core.setOutput('report', JSON.stringify(result));
-        core.summary
-            .addHeading('Action Results')
-            .addTable([
-            ['Field', 'Value'],
-            ['nodejsVersion', inputs.nodejsVersion]
-        ])
-            .write();
+        const nvmDir = path_1.default.resolve(inputs.projectPath);
+        core.info(`projectPath:  ${nvmDir}`);
     }
     catch (error) {
         core.setFailed(error instanceof Error ? error.message : 'Unknown error');
--- build-npm/src/main.ts
+++ build-npm/src/main.ts
@@ -8,13 +8,10 @@ import os from 'os'
 
 
 type InputParams = {
-  nvmVersion: string,
-  nodejsVersion: string,
+  projectPath: string
 }
 
 function validateInputs(params: Partial<InputParams>): InputParams {
-  if (!params.nvmVersion) throw new Error('nvmVersion input is required') ;
-  if (!params.nodejsVersion) throw new Error('nodejsVersion input is required') ;
   return params as InputParams
 }
 
@@ -22,83 +19,13 @@ async function run(): Promise<void> {
   try {
 
     const inputs = validateInputs({
-      nvmVersion: core.getInput('nvm-version', { required: true }),
-      nodejsVersion: core.getInput('nodejs-version', { required: true }),
-    })
+      projectPath: core.getInput('project-path', { required: true })
+    }) ;
 
-    const nvmDir = path.join(os.homedir(), '.nvm');
-    core.exportVariable('NVM_DIR', nvmDir);
-
-    const nvm = `curl -o install.sh https://raw.githubusercontent.com/nvm-sh/nvm/v${inputs.nvmVersion}/install.sh` ;
-    await exec.exec(nvm, []);
-    await exec.exec('bash', ['install.sh']);
-
-    // 加载 NVM 环境
-    await exec.exec('bash', [
-      '-c',
-      `. ${nvmDir}/nvm.sh && nvm install ${inputs.nodejsVersion} && nvm use ${inputs.nodejsVersion} `
-    ]);
-    
-    // 获取 Node.js 路径并添加到 PATH
-    const nodePath = await exec.getExecOutput('bash', [
-      '-c',
-      `. ${nvmDir}/nvm.sh && dirname $( nvm which ${inputs.nodejsVersion} ) `
-    ], {
-      silent: true
-    });
-    console.log(`nodePath: ${nodePath.stdout}`);
-    
-    const nodeBinPath = path.join(nodePath.stdout.trim(), 'bin');
-    core.addPath(nodeBinPath);
-
-    const textGet = await getText('node', ['-v']);
-    console.log(textGet);
-    core.info(`Node.js Of by GetText:   ` + textGet);
-
-    await exec.exec('node', ['-v']);
-
-    // const url = 'https://download.java.net/java/GA/jdk17/0d483333a00540d886896bac774ff48b/35/GPL/openjdk-17_linux-x64_bin.tar.gz';
-
-    // await exec.exec('wget', ['-q', url]);
-
-    // await exec.exec('tar', ['-xf', 'openjdk-17_linux-x64_bin.tar.gz', '-C', './']);
-
-    // const jdkHome = await capture('pwd', [])  + '/jdk-17' ;
-
-    // await exec.exec('chmod', ['+x', `${jdkHome}/bin/java`]);
+    const nvmDir = path.resolve(inputs.projectPath);
+    core.info(`projectPath:  ${nvmDir}`);
 
 
-// 设置 JAVA_HOME 环境变量
-// core.exportVariable('JAVA_HOME', jdkHome);
-  
-// 设置其他变量（如 PATH）
-// core.exportVariable('PATH', `${jdkHome}/bin:${process.env.PATH}`);
-
-
-    // await exec.exec('java', ['-version']);
-
-    // await exec.exec('ls', ['./'])
-
-    // await exec.exec('npm', ['i', 'npm@latest'])
-
-  
-
-    core.info(`Processing with: ${JSON.stringify(inputs)}`)
-    
-    const result = {
-      original: inputs,
-      processedAt: new Date().toISOString(),
-      message: `Received ${inputs.nodejsVersion} with 1 selection`
-    }
-
-    core.setOutput('report', JSON.stringify(result))
-    core.summary
-      .addHeading('Action Results')
-      .addTable([
-        ['Field', 'Value'],
-        ['nodejsVersion', inputs.nodejsVersion]
-      ])
-      .write()
   } catch (error: any) {
        core.setFailed(error instanceof Error ? error.message : 'Unknown error') ;
        throw new Error(error);
