f8fabd92432c4bee6f09ca4d209b168ca9cd2c1c
michael <wzjerrywz@163.com>
2025-06-09 10:19:55 +0800

    update

--- setup-python/src/main.ts
+++ setup-python/src/main.ts
@@ -1,16 +1,8 @@
 import * as core from '@actions/core'
 
-import * as tc from '@actions/tool-cache'
-
 import * as exec from '@actions/exec'
 
-import {  getText } from './common/cmd';
-
-import { exec as cpexe } from 'child_process';
-
-
-import path from 'path'
-import os from 'os'
+import { downloadConda, configConda, createVirtualEnv } from './step/Step';
 
 type InputParams = {
   nvmVersion: string,
@@ -23,34 +15,6 @@ function validateInputs(params: Partial<InputParams>): InputParams {
   return params as InputParams
 }
 
-async function initConda(): Promise<void> {
-  try {
-    const commands = [
-      'source ~/.bashrc',        // 加载 bash 配置
-      'conda activate github_actions_env',  // 激活环境
-      'conda info',             // 验证环境
-      'python --version'        // 验证 Python 版本
-    ].join(' && ');
-    
-    const { stdout, stderr } = await cpexe(`bash -l -c "${commands}"`);
-    console.log(stdout);
-    
-    if (stderr) {
-      console.warn('警告:', stderr);
-    }
-    
-    console.log('Conda 环境已成功初始化并激活!');
-  } catch (error) {
-    if (error instanceof Error) {
-      console.error('初始化 Conda 时出错:', error.message);
-      throw error;
-    } else {
-      console.error('未知错误:', error);
-      throw new Error(`初始化 Conda 失败: ${error}`);
-    }
-  }
-}
-
 async function run(): Promise<void> {
   try {
 
@@ -59,69 +23,24 @@ async function run(): Promise<void> {
     //   nodejsVersion: core.getInput('nodejs-version', { required: true }),
     // })
 
-    const condaVersion = 'py39_25.3.1-1'
-    const condaUrl = `https://repo.anaconda.com/miniconda/Miniconda3-${condaVersion}-Linux-x86_64.sh` ;
-    // 下载 Conda 安装程序
-    core.startGroup('下载 Conda 安装程序 ,  版本: ${condaVersion}');
-    const soft = 'soft/conda.sh';
-    const condaInstallerPath = await tc.downloadTool(condaUrl, './' + soft);
-    core.info(`Conda  ${condaVersion} 安装程序已下载到 :   ${condaInstallerPath}`);
-    const nowdir = await getText('pwd', []);
-    core.info(`当前目录 nowdir : ${nowdir}`);
-    await exec.exec('ls', ['-l', nowdir + '/' + soft]);
-    core.endGroup();
+    // step1 下载安装包
+    await downloadConda();
 
-    // 安装 Conda
-    core.startGroup('安装 Conda');
-    const down = await getText('pwd', []) + '/' + soft;
-    const condaDir = path.join(os.homedir(), 'miniconda3');
-    await exec.exec('bash', [
-      down,
-      '-b',
-      '-p', 
-      condaDir
-    ]);
-   // 验证 Conda 安装
-   await exec.exec('conda', ['--version']);
+    // step2 安装 nvm
+    await configConda();
    
-
-   const pythonVersion = '3.9.18' ;
- // 创建环境并安装指定版本的 Python
-    core.startGroup(`创建 Conda 环境并安装 Python ${pythonVersion}`);
+    // step3. 创建虚拟环境
+    await createVirtualEnv();
+   
     const envName = 'github_actions_env';
-    await exec.exec('conda', [
-      'create',
-      '-y',
-      '-q',
-      '-n', 
-      envName,
-      `python=${pythonVersion}` ,
-      '>/dev/null',
-      '2>&1'
-    ]);
-    core.info(`Python ${pythonVersion} 已安装到环境 ${envName}`);
-    core.endGroup();
-
- // 激活环境并配置 PATH
- const envBinDir = path.join(condaDir, 'envs', envName, 'bin');
- 
-core.addPath(envBinDir);
-core.info(`已将 ${envBinDir} 添加到 PATH`);
-
-    core.startGroup(`指定环境 ${envName} `);
-
-    // conda 设置环境
   
-    // await initConda();
     
-    console.log('Conda 环境已成功初始化并激活!');
+    console.log('验证!');
 
         // 验证 Python 安装
     await exec.exec(`conda run -n ${envName} python --version`, [ ]);
     await exec.exec(`conda run -n ${envName} pip --version`, [ ]);
 
-
-    core.endGroup();
 
   } catch (error: any) {
        core.setFailed(String(error)) ;
---
+++ setup-python/src/step/Step.ts
@@ -0,0 +1,61 @@
+import * as core from '@actions/core'
+
+import * as tc from '@actions/tool-cache'
+
+import * as exec from '@actions/exec'
+
+import path from 'path'
+import os from 'os'
+
+export async function downloadConda() {
+    const condaVersion = 'py39_25.3.1-1'
+    const condaUrl = `https://repo.anaconda.com/miniconda/Miniconda3-${condaVersion}-Linux-x86_64.sh` ;
+    // 下载 Conda 安装程序
+    core.startGroup('下载 Conda 安装程序 ,  版本: ${condaVersion}');
+    const soft = 'soft/conda.sh';
+    const condaInstallerPath = await tc.downloadTool(condaUrl, './' + soft);
+    core.info(`Conda  ${condaVersion} 安装程序已下载到 :   ${condaInstallerPath}`);
+    await exec.exec('ls', ['-l',  './' + soft]);
+    core.endGroup();
+}
+
+export async function configConda() {
+     // 安装 Conda
+     core.startGroup('配置 Conda');
+     const down = './' + 'soft/conda.sh';
+     const condaDir = path.join(os.homedir(), 'miniconda3');
+     await exec.exec('bash', [
+       down,
+       '-b',
+       '-p', 
+       condaDir
+     ]);
+    // 验证 Conda 安装
+    await exec.exec('conda', ['--version']);
+    core.endGroup();
+}
+
+export async function createVirtualEnv() {
+    const pythonVersion = '3.9.18' ;
+    // 创建环境并安装指定版本的 Python
+       core.startGroup(`创建虚拟环境 Python 版本： ${pythonVersion}`);
+       const envName = 'github_actions_env';
+       await exec.exec('conda', [
+         'create',
+         '-y',
+         '-q',
+         '-n', 
+         envName,
+         `python=${pythonVersion}` ,
+         '>/dev/null',
+         '2>&1'
+       ]);
+       core.info(`Python ${pythonVersion} 已安装到环境 ${envName}`);
+        // 激活环境并配置 PATH
+        const condaDir = path.join(os.homedir(), 'miniconda3');
+        const envBinDir = path.join(condaDir, 'envs', envName, 'bin');
+        core.addPath(envBinDir);
+        core.info(`已将 ${envBinDir} 添加到 PATH`);
+        // end 
+       core.endGroup();
+}
