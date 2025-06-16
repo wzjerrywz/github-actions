c4590f0693895152c591f140820d631f9aa15ec5
michael <wzjerrywz@163.com>
2025-06-16 17:32:01 +0800

    refactor: remove sensitive information and use environment variables

---
+++ repo-checkout/src/step/Windows.ts
@@ -0,0 +1,45 @@
+import { InputParamsType } from "../types/InputParamsType";
+import * as core from '@actions/core';
+import * as exec from '@actions/exec';
+import { FileSystem } from '../common/FileSystem';
+const { deleteDirectoryOrFile, createDir, listDir } = FileSystem;
+
+export async function gitCloneComplex(inputs: Partial<InputParamsType>) {
+    await deleteDirectoryOrFile('./repo');
+    await createDir('./repo');
+    process.chdir("./repo");
+
+    core.startGroup(`git clone 复杂操作`);
+
+    const [owner, repo] = inputs.repoName!.split('/');
+    console.log(`owner: ${owner}, repo: ${repo}`);
+
+    let USERNAME = '';
+    let TOKEN = '';
+    let URL = '';
+    switch (owner) {
+        case 'gitee':
+            USERNAME = process.env.GITEE_USERNAME || '';
+            TOKEN = process.env.GITEE_TOKEN || '';
+            URL = 'https://oauth2:<TOKEN>@gitee.com/<USERNAME>/<REPOSITORY>.git';
+            break;
+        case 'github':
+            USERNAME = process.env.GITHUB_USERNAME || '';
+            TOKEN = process.env.GITHUB_TOKEN || '';
+            URL = 'git@github.com:<USERNAME>/<REPOSITORY>.git';
+            break;
+        case 'gitlab':
+            break;
+        default:
+            break;
+    }
+
+    const gitUrl = URL.replace('<USERNAME>', USERNAME)
+                     .replace('<TOKEN>', TOKEN)
+                     .replace('<REPOSITORY>', repo);
+    
+    core.startGroup(`git clone -b ${inputs.branchName} ${gitUrl}`);
+    await exec.exec('git', ['clone', '-b', `${inputs.branchName}`, gitUrl]);
+    core.endGroup();
+    await listDir();
+} 
