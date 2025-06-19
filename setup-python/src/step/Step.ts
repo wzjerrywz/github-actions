4051b810722a8c8ad4d169aa971b5e64d111d702
michael <wzjerrywz@163.com>
2025-06-19 14:22:32 +0800

    update

--- setup-python/src/step/Step.ts
+++ setup-python/src/step/Step.ts
@@ -19,6 +19,7 @@ export class Step {
         const title = ` 下载 conda .sh , 版本 ： ${inputs.condaVersion} ` ;
         await this.groupWrapper(inputs, title, async ({ condaVersion }) => {
             const url = this.CONDA_URL.replace('<VERSION>', condaVersion! );
+            core.info(`下载地址 : ${url}`);
             const downloadPath = './soft/conda.sh';
             const condaInstallerPath = await tc.downloadTool(url, downloadPath);
             core.info(`Conda  ${condaVersion} 安装程序已下载到 :   ${condaInstallerPath}`);
