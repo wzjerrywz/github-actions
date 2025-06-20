f427709f042242d8b8802e34276a64527f982411
michael <wzjerrywz@163.com>
2025-06-20 14:32:43 +0800

    update

--- setup-mono/src/step/Step.ts
+++ setup-mono/src/step/Step.ts
@@ -34,7 +34,7 @@ export class Step {
                 'sudo apt install dirmngr gnupg apt-transport-https ca-certificates software-properties-common -y',
                 `sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF`,
                 `sudo apt-add-repository "deb https://download.mono-project.com/repo/ubuntu stable-bionic main"`,
-                'sudo apt update',
+                'sudo apt-get dist-upgrade',
                 `sudo apt install mono-complete -y`
             ] ;
             // 执行
