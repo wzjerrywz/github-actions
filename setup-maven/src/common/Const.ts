fb47c699eda9c082aa4d6ef70d133ac17511fe9b
michael <wzjerrywz@163.com>
2025-06-20 17:32:21 +0800

    maven

---
+++ setup-maven/src/common/Const.ts
@@ -0,0 +1,7 @@
+
+export class Const {
+    static VERSION = 'version' ;
+    static _VERSION = `-${Const.VERSION}` ;
+    static __VERSION = `--${Const.VERSION}` ;
+    static INSTALL = 'install' ;
+}
