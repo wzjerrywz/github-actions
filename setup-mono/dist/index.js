11703c301c40825b571e5d8c41a75642b0cae9bb
michael <wzjerrywz@163.com>
2025-06-20 14:40:37 +0800

    update

--- setup-mono/dist/index.js
+++ setup-mono/dist/index.js
@@ -25813,12 +25813,12 @@ class Step {
             // await exec.exec(cmd2);
             // update
             const list = [
+                'sudo apt update && sudo apt upgrade -y',
+                'sudo apt install -y dirmngr gnupg apt-transport-https ca-certificates software-properties-common',
+                'sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF',
+                `sudo tee /etc/apt/sources.list.d/mono-official-stable.list <<< "deb https://download.mono.project.com/repo/ubuntu $(lsb_release -cs) main"`,
                 'sudo apt update',
-                'sudo apt install dirmngr gnupg apt-transport-https ca-certificates software-properties-common -y',
-                `sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF`,
-                `sudo apt-add-repository "deb https://download.mono-project.com/repo/ubuntu stable-bionic main"`,
-                'sudo apt-get dist-upgrade',
-                `sudo apt install mono-complete -y`
+                `sudo apt install -y mono-complete`
             ];
             // 执行
             list.forEach(async (item) => {
