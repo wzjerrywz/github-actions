15b7bacf7b284978fd9045e48d5bf1938dccaeca
michael <wzjerrywz@163.com>
2025-06-20 14:26:45 +0800

    update

--- setup-mono/src/step/Step.ts
+++ setup-mono/src/step/Step.ts
@@ -30,8 +30,12 @@ export class Step {
             // await exec.exec(cmd2);
             // update
             const list = [
+                'sudo apt update',
                 'sudo apt install dirmngr gnupg apt-transport-https ca-certificates software-properties-common -y',
-                'sudo apt-get update',
+                `sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF`,
+                `sudo apt-add-repository "deb https://download.mono-project.com/repo/ubuntu stable-bionic main"`,
+                'sudo apt update',
+                `sudo apt install mono-complete -y`
             ] ;
             // 执行
             list.forEach(async (item) => {
