381c65a7ef75b1c863f984a194b11d3a1af80d6b
michael <wzjerrywz@163.com>
2025-06-20 17:51:34 +0800

    update

--- setup-maven/src/step/Step.ts
+++ setup-maven/src/step/Step.ts
@@ -15,13 +15,37 @@ export class Step {
 
     inputs: Partial<InputParamsType> | unknown ;
 
+    bigVersion = '3';
+
+    URL_TEMPLATE = 'https://archive.apache.org/dist/maven/maven-<PREFIX>/<VERSION>/binaries/apache-maven-<VERSION>-bin.tar.gz' ;
+
     constructor() {
         this.validInputs();
     }
 
     // 整个流程
     async go() {
-        log(this.inputs);
+        await this.download();
+        await this.see();
+    }
+
+    // 下载
+    async download() {
+        const { mavenVersion } = this.inputs as InputParamsType;
+        await this.groupWrapper(` 下载 maven , 版本号 ${mavenVersion} `,  async () => {
+            const url = this.URL_TEMPLATE
+                            .replaceAll('<PREFIX>', this.bigVersion)
+                            .replaceAll('<VERSION>', mavenVersion);
+            await tc.downloadTool(url, path.resolve("./soft/maven", `mvn${mavenVersion}.tar.gz`));
+        });
+    }
+
+    // 查看
+    async see() {
+        const { mavenVersion } = this.inputs as InputParamsType;
+        await this.groupWrapper(` 查看 `,  async () => {
+            await exec.exec(`ls -l ./soft/maven`);
+        });
     }
 
 
@@ -37,11 +61,11 @@ export class Step {
 
 
     // 组装函数
-    async  groupWrapper(inputs: Partial<InputParamsType>, title: string, fn: (inputs: Partial<InputParamsType>) => Promise<void>) {
+    async  groupWrapper(title: string, fn: () => Promise<void>) {
         // start group
         core.startGroup(title);
         // 执行函数
-        await fn(inputs);
+        await fn();
         // end group
         core.endGroup();
     };
