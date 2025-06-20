46cc8dc88cfd2eff130544b1f156cdc3001a0be6
michael <wzjerrywz@163.com>
2025-06-20 13:39:28 +0800

    update

---
+++ setup-mono/dist/common/Conf.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Conf = void 0;
+class Conf {
+    static KEY_SERVER = 'hkp://keyserver.ubuntu.com:80';
+    static RECV_KEYS = '3FA7E0328081BFF6A14DA29AA6A19B38D3D831';
+    static DOWNLOAD_URL = 'https://download.mono-project.com/repo/ubuntu';
+    static ETC_CONFIG = '/etc/apt/sources.list.d/mono-official.list';
+    // 
+    static URL_1 = `sudo apt-key adv --keyserver <KEY_SERVER> --recv-keys <RECV_KEYS>`;
+    static URL_2 = `echo "deb <DOWNLOAD_URL> stable-focal/snapshots/<MONO_VERSION> main" | sudo tee <ETC_CONFIG>`;
+}
+exports.Conf = Conf;
---
+++ setup-mono/dist/step/Step.js
@@ -0,0 +1,91 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || (function () {
+    var ownKeys = function(o) {
+        ownKeys = Object.getOwnPropertyNames || function (o) {
+            var ar = [];
+            for (var k in o) if (Object.prototype.hasOwnProperty.call(o, k)) ar[ar.length] = k;
+            return ar;
+        };
+        return ownKeys(o);
+    };
+    return function (mod) {
+        if (mod && mod.__esModule) return mod;
+        var result = {};
+        if (mod != null) for (var k = ownKeys(mod), i = 0; i < k.length; i++) if (k[i] !== "default") __createBinding(result, mod, k[i]);
+        __setModuleDefault(result, mod);
+        return result;
+    };
+})();
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Step = void 0;
+const core = __importStar(require("@actions/core"));
+const exec = __importStar(require("@actions/exec"));
+const Const_1 = require("../common/Const");
+const { __VERSION, INSTALL, VERSION } = Const_1.Const;
+const Conf_1 = require("../common/Conf");
+const { KEY_SERVER, RECV_KEYS, DOWNLOAD_URL, ETC_CONFIG, URL_1, URL_2 } = Conf_1.Conf;
+class Step {
+    // step1. 配置 mono 源
+    async configRepo(inputs) {
+        const title = `配置 mono 源：${inputs.monoVersion}`;
+        await this.groupWrapper(inputs, title, async ({ monoVersion }) => {
+            const cmd1 = URL_1.replaceAll('<KEY_SERVER>', KEY_SERVER)
+                .replaceAll('<RECV_KEYS>', RECV_KEYS);
+            const cmd2 = URL_2.replaceAll('<DOWNLOAD_URL>', DOWNLOAD_URL)
+                .replaceAll('<MONO_VERSION>', monoVersion)
+                .replaceAll('<ETC_CONFIG>', ETC_CONFIG);
+            // 执行
+            await exec.exec(cmd1);
+            await exec.exec(cmd2);
+            // update
+            await exec.exec('sudo apt-get update');
+        });
+    }
+    ;
+    // step2. 安装 mono 
+    async install(inputs) {
+        const title = `安装 mono : ${inputs.monoVersion}`;
+        await this.groupWrapper(inputs, title, async ({ monoVersion }) => {
+            // 安装 Mono
+            // sudo apt install -y mono-complete=6.12.0.122*
+            const params = [INSTALL, '-y', `mono-complete=${monoVersion}*`];
+            await exec.exec('sudo apt', params);
+        });
+    }
+    ;
+    // step3. 查看 mono 版本
+    async checkVersion(inputs) {
+        const title = `查看 mono 版本`;
+        await this.groupWrapper(inputs, title, async ({}) => {
+            await exec.exec('mono', [__VERSION]);
+        });
+    }
+    ;
+    // 组装函数
+    async groupWrapper(inputs, title, fn) {
+        // start group
+        core.startGroup(title);
+        // 执行函数
+        await fn(inputs);
+        // end group
+        core.endGroup();
+    }
+    ;
+}
+exports.Step = Step;
