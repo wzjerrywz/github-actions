3c4fe70205b1ecfd0626e4d7aa30558e70a71d6a
michael <wzjerrywz@163.com>
2025-06-20 13:38:39 +0800

    mono

---
+++ setup-mono/src/step/Step.ts
@@ -0,0 +1,68 @@
+import { InputParamsType } from "../types/InputParamsType";
+
+import * as child_process from 'child_process';
+
+
+import * as core from '@actions/core';
+import * as exec from '@actions/exec';
+import * as tc from '@actions/tool-cache';
+import * as path from 'path';
+
+import { Const } from '../common/Const';
+const { __VERSION, INSTALL, VERSION } = Const;
+
+import { Conf } from '../common/Conf';
+const { KEY_SERVER, RECV_KEYS, DOWNLOAD_URL, ETC_CONFIG, URL_1, URL_2 } = Conf;
+
+export class Step {
+
+    // step1. 配置 mono 源
+    async configRepo(inputs: Partial<InputParamsType>) {
+        const title = `配置 mono 源：${inputs.monoVersion!}` ;
+        await this.groupWrapper(inputs, title, async ({ monoVersion }) => {
+            const cmd1 = URL_1.replaceAll('<KEY_SERVER>', KEY_SERVER)
+                               .replaceAll('<RECV_KEYS>', RECV_KEYS);
+            const cmd2 = URL_2.replaceAll('<DOWNLOAD_URL>', DOWNLOAD_URL)
+                               .replaceAll('<MONO_VERSION>', monoVersion!)
+                               .replaceAll('<ETC_CONFIG>', ETC_CONFIG);
+            // 执行
+            await exec.exec(cmd1);
+            await exec.exec(cmd2);
+            // update
+            await exec.exec('sudo apt-get update');
+        });
+    };
+
+
+        // step2. 安装 mono 
+        async install(inputs: Partial<InputParamsType>) {
+            const title = `安装 mono : ${inputs.monoVersion!}` ;
+            await this.groupWrapper(inputs, title, async ({ monoVersion }) => {
+                // 安装 Mono
+                // sudo apt install -y mono-complete=6.12.0.122*
+                const params = [ INSTALL , '-y', `mono-complete=${monoVersion!}*`];
+                await exec.exec('sudo apt', params);
+            });
+        };
+
+        // step3. 查看 mono 版本
+        async checkVersion(inputs: Partial<InputParamsType>) {
+            const title = `查看 mono 版本` ;
+            await this.groupWrapper(inputs, title, async ({  }) => {
+                await exec.exec('mono', [__VERSION]);
+            });
+        };
+
+
+    // 组装函数
+    async  groupWrapper(inputs: Partial<InputParamsType>, title: string, fn: (inputs: Partial<InputParamsType>) => Promise<void>) {
+        // start group
+        core.startGroup(title);
+        // 执行函数
+        await fn(inputs);
+        // end group
+        core.endGroup();
+    };
+
+
+}
