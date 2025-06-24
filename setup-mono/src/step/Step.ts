7ed37eac24b400a8739b0ac551cec119d7a2acac
michael <wzjerrywz@163.com>
2025-06-24 11:13:39 +0800

    update

--- setup-mono/src/step/Step.ts
+++ setup-mono/src/step/Step.ts
@@ -22,90 +22,46 @@ export class Step {
     }
 
     async go() {
-        // await this.configRepo();
-        // await this.downloadMono();
-        // await this.extract();
-        // await this.install();
-        const url = 'https://download.mono-project.com/sources/mono/mono-6.12.0.199.tar.xz';
-        await exec.exec('curl -L -o mono-6.12.0.199.tar.xz ' + url);
-          // 3. 下载并缓存
-            // const downloadPath = await tc.downloadTool(url);
-            // const extractDir = await tc.extractTar(downloadPath, '-xJ');
-            // process.chdir(downloadPath));
-
-            // 解压
-            await exec.exec('pwd');
-            await exec.exec('ls -l ./');
-            await exec.exec('tar -xf mono-6.12.0.199.tar.xz -C ./');
-            await exec.exec('ls -l ./');
-
-   
-
-            // 4. 缓存目录，方便复用
-            // const monoPath = await tc.cacheDir('./', 'mono', '6.12.0.199');
-
-            // 5. 添加到 PATH
-            const binDir = path.join(path.resolve('./mono-6.12.0.199'), 'bin');
-            core.addPath(binDir);
-            core.info(`Mono added to PATH: ${binDir}`);
-
-            // 6. 验证安装
-            let output = '';
-            await exec.exec('mono', ['--version'], {
-            listeners: {
-                stdout: (data: Buffer) => { output += data.toString(); }
-            }
-            });
-            core.info(`mono --version:\n${output}`);
-            
+        await this.download();
+        await this.tar();
+        await this.env();
+        await this.see();   
     }
 
-    async configRepo() {
-        const title = `配置 mono 源： ${this.inputs.monoVersion!}` ;
-        await this.groupWrapper(title, async () => {
-            await exec.exec('ls -l ./');
-            await exec.exec('pwd');
-        });
-    }
-
-    async downloadMono() {
+    async download() {
         const { monoVersion } = this.inputs;
-            await this.groupWrapper(`下载 `,  async () => {
+            await this.groupWrapper(`下载 版本：  ${monoVersion} `,  async () => {
                 const url = this.DOWNLOAD_URL.replaceAll('<VERSION>', monoVersion!);
-                await tc.downloadTool(url, path.resolve("./soft/mono"!, `mono-${monoVersion!}.tar.xz`));
-                //
-                process.chdir(path.resolve("./soft/mono", ``));
-                await exec.exec('ls -l ./');
-                await exec.exec('pwd');
+                await exec.exec(`curl -L -o mono-${monoVersion!}.tar.xz  ` + url);
         });
     }
 
 
     // 解压
-    async extract() {
+    async tar() {
         const { monoVersion } = this.inputs;
             await this.groupWrapper(`解压  ： .tar.xz `,  async () => {
-                const name = `mono-${monoVersion!}.tar.xz`
-                await exec.exec(`tar -xf ${name} -C ./`);
-                //
-                await exec.exec('ls -l ./');
-                await exec.exec('pwd');
+                // 解压
+                await exec.exec(`tar -xf mono-${monoVersion!}.tar.xz -C ./`);
         });
     }
 
-    // install
-    async install() {
+    // 配置环境变量
+    async env() {
         const { monoVersion } = this.inputs;
-            await this.groupWrapper(`安装 mono ： ${monoVersion}`,  async () => {
-                process.chdir(`./mono-${monoVersion!}`);
-                const list = [
-                    './configure --prefix=/usr/local && make && make install'
-                ] ;
-                list.forEach(async (item) => {
-                    await exec.exec(item);
-                });
-                // 查看版本
-                await exec.exec('mono --version');
+            await this.groupWrapper(`配置环境变量`,  async () => {
+                const binDir = path.join(path.resolve(`./mono-${monoVersion!}`), 'bin');
+                core.addPath(binDir);
+                // info
+                core.info(`Mono added to PATH: ${binDir}`);
+        });
+    }
+
+      // 查看版本
+      async see() {
+        const { monoVersion } = this.inputs;
+            await this.groupWrapper(`查看版本`,  async () => {
+                await exec.exec('mono', [__VERSION]);
         });
     }
 
